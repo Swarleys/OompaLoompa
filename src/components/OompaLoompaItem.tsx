f212495ac2ca752e4b51a412ca11a6fc07629444
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2023-11-30 23:49:23 +0100

    Adding filter functionality

--- src/components/OompaLoompaItem.tsx
+++ src/components/OompaLoompaItem.tsx
@@ -7,13 +7,25 @@ interface OompaLoompaItemProps {
 
 export default function OompaLoompaItem({ oompaLoompa }: OompaLoompaItemProps) {
   return (
-    <Link to={`/${oompaLoompa.id}`} key={oompaLoompa.id} className="mx-auto max-w-[400px] ">
-      <img src={oompaLoompa.image} alt={oompaLoompa.first_name} />
+    <Link
+      to={`/${oompaLoompa.id}`}
+      key={oompaLoompa.id}
+      className="mx-auto max-w-[400px] "
+    >
+      <img
+        src={oompaLoompa.image}
+        loading={oompaLoompa.id < 4 ? "eager" : "lazy"}
+        width={350}
+        height={280}
+        alt={oompaLoompa.first_name}
+      />
       <p className="text-2xl text-slate-800">
         {oompaLoompa.first_name} {oompaLoompa.last_name}
       </p>
-      <p className="text-slate-500" >{oompaLoompa.gender === "F" ? "Woman" : "Man"}</p>
-      <p className="text-slate-500" >{oompaLoompa.profession}</p>
+      <p className="text-slate-500">
+        {oompaLoompa.gender === "F" ? "Woman" : "Man"}
+      </p>
+      <p className="text-slate-500">{oompaLoompa.profession}</p>
     </Link>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,24 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
   const { data: oompaLoompaResponse, isLoading } =
     useGetOompaLoompasQuery(page);
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+  
+  const filteredOompaLoompas = oompaLoompaResponse?.results.filter(
+    (oompaLoompa) => {
+      return `${oompaLoompa.first_name} ${oompaLoompa.last_name} ${oompaLoompa.profession}`
+        .toLowerCase()
+        .includes(search.toLowerCase());
+    }
+  );
+
+  // Error handling, I can't use the isError property from RTK Query because when is failing the request the data is not undefined, that's why I'm using the stackTrace property to check if the request is failing
   if (
     oompaLoompaResponse &&
     Object.prototype.hasOwnProperty.call(oompaLoompaResponse, "stackTrace")
@@ -26,18 +41,38 @@ export default function Home() {
 
   return (
     <main>
-      <header className="flex flex-col justify-center items-center gap-4 mb-12">
+      <header className="flex flex-col justify-center items-center gap-4 mb-12 md:max-w-[900px] lg:max-w-[1200px] m-auto">
+        <search role="search" className="self-end mr-4">
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className="flex flex-col gap 4px"
+          >
+            <input
+              type="search"
+              onChange={handleSearch}
+              value={search}
+              id="search"
+              name="search"
+              className="border border-gray-300 rounded-md p-2"
+              aria-label="Busqueda de Oompa Loompas"
+              placeholder="Busqueda de Oompa..."
+            />
+          </form>
+        </search>
         <h1 className="text-6xl text-slate-700">Find your Oompa Loompa</h1>
-        <h3 className="text-4xl text-slate-500">There are more than 100k</h3>
+        <h2 className="text-4xl text-slate-500">There are more than 100k</h2>
       </header>
       {isLoading ? (
         <div>Cargando resultados...</div>
       ) : (
-        <section className="flex justify-center mx-4 m-auto md:max-w-[900px] lg:max-w-[1200px]">
+        <section className="flex m-auto md:max-w-[900px] lg:max-w-[1200px]">
           <div className="mx-4 w-full grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-4">
-            {oompaLoompaResponse &&
-              oompaLoompaResponse?.results.map((oompaLoompa) => (
-                <OompaLoompaItem oompaLoompa={oompaLoompa} />
+            {filteredOompaLoompas &&
+              filteredOompaLoompas?.map((oompaLoompa) => (
+                <OompaLoompaItem
+                  key={oompaLoompa.id}
+                  oompaLoompa={oompaLoompa}
+                />
               ))}
           </div>
         </section>
