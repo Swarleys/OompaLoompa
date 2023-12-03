01dde35f7a13b4f38260be4f563dca41481b5c51
Esteban Ramos Coronado <esteban@Mac-mini-de-Esteban.local>
2023-12-03 09:38:23 +0100

    Adding vitest --ui, and fix globals so I don't need to import in every test

--- src/components/Error/Error.test.tsx
+++ src/components/Error/Error.test.tsx
@@ -1,22 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Error from "./Error";
-import '@testing-library/jest-dom';
-import { expect, test } from "vitest";
+import "@testing-library/jest-dom";
 
+describe("Error component", () => {
+  test("Renders Error", () => {
+    render(
+      <Router>
+        <Error />
+      </Router>
+    );
 
-test("renders Error component", () => {
-  render(
-    <Router>
-      <Error />
-    </Router>
-  );
+    const errorText = screen.getByText(/An error has ocurred ❌/i);
+    expect(errorText).toBeInTheDocument();
 
-  const errorText = screen.getByText(/An error has ocurred ❌/i);
-  expect(errorText).toBeInTheDocument();
-
-  const linkElement = screen.getByText(/Go back to the landing page ⬅️/i);
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toHaveAttribute("href", "/");
+    const linkElement = screen.getByText(/Go back to the landing page ⬅️/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href", "/");
+  });
 });
-
--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -2,26 +2,27 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./NavBar";
 import "@testing-library/jest-dom";
-import { expect, test } from "vitest";
 
-test("renders NavBar component", () => {
-  render(
-    <Router>
-      <NavBar />
-    </Router>
-  );
+describe("NavBar component", () => {
+  test("Renders NavBar component", () => {
+    render(
+      <Router>
+        <NavBar />
+      </Router>
+    );
 
-  const logoElement = screen.getByAltText(/Logo Oompa Loompa/i);
-  expect(logoElement).toBeInTheDocument();
-  expect(logoElement).toHaveAttribute(
-    "src",
-    "https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png"
-  );
+    const logoElement = screen.getByAltText(/Logo Oompa Loompa/i);
+    expect(logoElement).toBeInTheDocument();
+    expect(logoElement).toHaveAttribute(
+      "src",
+      "https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png"
+    );
 
-  const textElement = screen.getByText(/Oompa Loompa's Crew/i);
-  expect(textElement).toBeInTheDocument();
+    const textElement = screen.getByText(/Oompa Loompa's Crew/i);
+    expect(textElement).toBeInTheDocument();
 
-  const linkElement = screen.getByRole("link");
-  expect(linkElement).toBeInTheDocument();
-  expect(linkElement).toHaveAttribute("href", "/");
+    const linkElement = screen.getByRole("link");
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href", "/");
+  });
 });
